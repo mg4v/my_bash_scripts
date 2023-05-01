edf531d4a2a568b304a9da336dd3e3835e8f737b
mgx <i@mgorya4ev.ru>
2023-05-01 11:45:19 +0300

    add soft 2 InstallSpoOnUbuntu.sh

--- InstallSpoOnUbuntu.sh
+++ InstallSpoOnUbuntu.sh
@@ -4,4 +4,6 @@
 apt update && \
 apt install \
 git mc htop bashtop neofetch tmux lynx vim nano wget \
-openssh-server net-tools ubuntu-restricted-extras dconf-tools -y
+openssh-server net-tools ubuntu-restricted-extras dconf-tools \
+flameshot p7zip-rar rar unrar unace arj cabextract \
+gnome-shell-extension-manager-y
