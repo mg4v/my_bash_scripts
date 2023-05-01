74dc41042e5e5b763f5e24a09043f25c12548244
mgx <i@mgorya4ev.ru>
2023-05-01 12:22:21 +0300

    correct

--- installVdaNvidia.sh
+++ installVdaNvidia.sh
@@ -4,9 +4,9 @@ nvidia_vers=`ubuntu-drivers devices | grep -i recommended`
 
 if lspci | grep -i nvidia ; then
   echo "The system has an NVIDIA graphics card"
-  echo
+  echo "<------>"
   read -p "Do you want to install the video $nvidia_vers version?" -n 1 -r
-  #echo    # (optional) move to a new line
+  echo    # (optional) move to a new line
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
    ubuntu-drivers autoinstall -y
